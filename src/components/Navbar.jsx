0713956ef06503b87cb6330ff41c817382da32ac
Shah Taimoor <taimour448@gmail.com>
2024-12-05 00:42:00 +0500

    create Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
+
 const Navbar = () => {
-  return (
-    <nav>
-      <div>
-        <h3>{new Date().toUTCString().slice(0,16)}</h3>
-        <h1>Shah Taimoor</h1>
-      </div>
-      <div>
-        <input type="search" name='search' placeholder='Search Here'/>
-      </div>
-    </nav>
-  )
+    return (
+        <nav className='flex justify-between items-center mx-6 mt-3 '>
+            <div>
+                <h3 className='text-xl'>{new Date().toUTCString().slice(0, 16)}</h3>
+                <h1 className='text-gray-500 font-bold text-3xl'>Ecomerce Food App</h1>
+            </div>
+            <div>
+                <input className='p-3 border rounded-lg w-full md:w-[300px]' type="search" name='search' placeholder='Search Here' />
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
