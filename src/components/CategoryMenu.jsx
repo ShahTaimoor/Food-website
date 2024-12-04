acde1c566b4029b2696c858f2ae9517d0a55c4a4
Shah Taimoor <taimour448@gmail.com>
2024-12-05 00:54:09 +0500

    create category

---
+++ src/components/CategoryMenu.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const CategoryMenu = () => {
+  return (
+    <section className='mx-6 mt-5'>
+        <h3 className='text-xl text-gray-500 font-semibold'>Food Menu</h3>
+           <div className='my-5 flex gap-3'>
+            <button className='bg-gray-400 font-bold px-5 hover:text-white duration-150 transition-all ease-in-out hover:bg-green-500 rounded-lg py-1.5'>
+                All
+            </button>
+            <button className='bg-gray-400 font-bold px-5 hover:text-white duration-150 transition-all ease-in-out hover:bg-green-500 rounded-lg py-1.5'>
+                BreakFast
+            </button>
+            <button className='bg-gray-400 font-bold px-5 hover:text-white duration-150 transition-all ease-in-out hover:bg-green-500 rounded-lg py-1.5'>
+                Chicken
+            </button>
+            <button className='bg-gray-400 font-bold px-5 hover:text-white duration-150 transition-all ease-in-out hover:bg-green-500 rounded-lg py-1.5'>
+                Lunch
+            </button>
+            
+           </div>
+    </section>
+  )
+}
+
+export default CategoryMenu
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import CategoryMenu from '../components/CategoryMenu'
 
 const Home = () => {
-  return (
-    <div>
-        <Navbar/>
-        
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <CategoryMenu />
+            
+        </div>
+    )
 }
 
 export default Home
